95739ebe7ae1053c84b3b225cec3ce40d8b64651
Soham Choudhuri <sohamchoudhuri2003@gmail.com>
2025-01-21 22:10:04 +0530

    Update Quiz_Management_System.java

--- src/quiz_management_system/Quiz_Management_System.java
+++ src/quiz_management_system/Quiz_Management_System.java
@@ -14,7 +14,6 @@ public class Quiz_Management_System {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
